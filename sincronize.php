431fcdc045210a240708e470b5fc7e2892922e87
FilipeJohansson <filipejohansson7@gmail.com>
2021-08-12 18:55:33 -0300

    remove unused code

--- sincronize.php
+++ sincronize.php
@@ -1,12 +1,12 @@
 <?php
-    require_once("./DAOs/MusicDAO.php");
+require_once("./DAOs/MusicDAO.php");
 
-    $musicDAO = new MusicDAO();
+$musicDAO = new MusicDAO();
 
-    // get all titles to fill autocomplete tags
-    $titles = $musicDAO->getMusicsTitle();
-    
-    /* get a music from a title
+// get all titles to fill autocomplete tags
+$titles = $musicDAO->getMusicsTitle();
+
+/* get a music from a title
     *  Music = {
     *      $id;
     *      $videoId;
@@ -15,34 +15,37 @@
     *      $musicTimes;
     *  }
     */
-    $music = $musicDAO->getMusicFromTitle("No lives matter");
+$music = $musicDAO->getMusicFromTitle("No lives matter");
 
-    if(is_int($music) || is_int($titles)) {
-        if($music == -1) { /* ERROR */ }
-        if($titles == -1) { /* ERROR */ }
+if (is_int($music) || is_int($titles)) {
+    if ($music == -1) { /* ERROR */
     }
+    if ($titles == -1) { /* ERROR */
+    }
+}
 
-    // break lyric into an array by the separator
-    $lyric = explode(';', $music->lyric);
+// break lyric into an array by the separator
+$lyric = explode(';', $music->lyric);
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="author" content="Filipe Johansson">
+    <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Filipe Johansson">
     <meta name="title" content="Prism">
 
-	<link rel="icon" href="./assets/icon.png">
+    <link rel="icon" href="./assets/icon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet">
-	<title>Prism</title>
-    
-	<!--CSS-->
+    <title>Prism</title>
+
+    <!--CSS-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/jquery-ui.css">
@@ -53,20 +56,21 @@
     <script src="./js/jquery-ui.js"></script>
 
     <script>
-        $( function() {
+        $(function() {
             var availableTags = [
                 <?php
-                    if($titles != -1)
-                        foreach ($titles as $t)
-                            echo '"' . $t . '",';    
+                if ($titles != -1)
+                    foreach ($titles as $t)
+                        echo '"' . $t . '",';
                 ?>
             ];
-            $( "#input_search" ).autocomplete({
+            $("#input_search").autocomplete({
                 source: availableTags
             });
-        } );
+        });
     </script>
 </head>
+
 <body>
 
     <header class="mb-4">
@@ -98,12 +102,13 @@
                         // 3. This function creates an <iframe> (and YouTube player)
                         //    after the API code downloads.
                         var player;
+
                         function onYouTubeIframeAPIReady() {
                             player = new YT.Player('player', {
                                 width: '100%',
                                 videoId: '<?php echo $music->videoId; ?>',
                                 playerVars: {
-                                    'playsinline': 0, 
+                                    'playsinline': 0,
                                     'rel': 0,
                                     'controls': 1,
                                     'showinfo': 0,
@@ -147,12 +152,12 @@
             var white = [255, 255, 255];
             var black = [0, 0, 0];
 
-            for(x = 0; x != 1; x++) {
+            for (x = 0; x != 1; x++) {
                 var rgb = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
-                if(rgb === white || rgb === black)
+                if (rgb === white || rgb === black)
                     x = 1;
             }
-            
+
             return rgb;
         }
 
@@ -163,16 +168,16 @@
 
     <script>
         var lyric = [
-            <?php 
-                foreach ($lyric as $l) {
-                    echo '"' . $l . '",' . PHP_EOL;
-                } 
+            <?php
+            foreach ($lyric as $l) {
+                echo '"' . $l . '",' . PHP_EOL;
+            }
             ?>
         ];
-        
+
         var lyricTimes = [
             <?php
-                echo $music->times;
+            echo $music->times;
             ?>
         ];
 
@@ -180,7 +185,7 @@
             actualLine: 0,
             lineChanged: function(value) {},
             set line(value) {
-                if(this.actualLine != value) {
+                if (this.actualLine != value) {
                     this.actualLine = value;
                     this.lineChanged();
                 }
@@ -219,33 +224,33 @@
 
         var y = false;
         setInterval(function() {
-            if(player.getPlayerState() == 2) {
+            if (player.getPlayerState() == 2) {
                 pause(true);
             }
 
-            if(!paused) {
+            if (!paused) {
                 videoCurrentTime = player.playerInfo.currentTime;
 
-                for(var x = 0; x <= lyricTimes.length + 1; x += 2) {
-                    if(videoCurrentTime >= lyricTimes[x] && videoCurrentTime <= lyricTimes[x + 1]) {
-                        if(x == 0) {
+                for (var x = 0; x <= lyricTimes.length + 1; x += 2) {
+                    if (videoCurrentTime >= lyricTimes[x] && videoCurrentTime <= lyricTimes[x + 1]) {
+                        if (x == 0) {
                             line.line = 0;
                         } else {
                             line.line = x / 2;
                         }
-                        
+
                         break;
                     }
 
-                    if(videoCurrentTime > lyricTimes[lyricTimes.length - 1]) {
-                        if(!y) {
+                    if (videoCurrentTime > lyricTimes[lyricTimes.length - 1]) {
+                        if (!y) {
                             goToLine(line.actualLine++);
                             y = true;
                         }
-                            
+
                         clearInterval(intervalId);
                     }
-                    
+
                 }
             }
 
@@ -266,7 +271,7 @@
 
             // just start a new one
             startInterval(interval, percent);
-            
+
         }
 
         function calculateProgressPercent(start, end, currentTime) {
@@ -280,12 +285,12 @@
         function startInterval(interval, percent) {
             progressbarVal = percent;
             // Store the id of the interval so we can clear it later
-            intervalId = setInterval(function () {
-                if(!paused) {
+            intervalId = setInterval(function() {
+                if (!paused) {
                     document.getElementById("progressbar").style.width = progressbarVal + "%";
                     progressbarVal++;
 
-                    if(progressbarVal >= 101) {
+                    if (progressbarVal >= 101) {
                         progressbarVal = 0;
                         clearInterval(intervalId);
                     }
@@ -296,25 +301,14 @@
         function fillLyric(lyric) {
             var HTMLLyrics = '';
 
-            var x = 0;
             lyric.forEach(function(item) {
-                switch (true) {
-                    case (lyric[line.actualLine] == item):
-                        if(item != "%") {
-                            HTMLLyrics += '<div class="row"><div class="col-sm-6"><input type="text" class="form-control" placeholder="' + lyricTimes[x * 2] + '"></input></div><div class="col-sm-6"><input type="text" class="form-control" placeholder="' + lyricTimes[x * 2 + 1] + '"></input></div></div>';
-                            HTMLLyrics += '<p class="active">' + item + '</p>';
-                        }
-                        break;
-
-                    default:
-                        if(item != "%") {
-                            HTMLLyrics += '<div class="row"><div class="col-sm-6"><input type="text" class="form-control" placeholder="' + lyricTimes[x * 2] + '"></input></div><div class="col-sm-6"><input type="text" class="form-control" placeholder="' + lyricTimes[x * 2 + 1] + '"></input></div></div>';
-                            HTMLLyrics += '<p>' + item + '</p>';
-                        }
-                        break;
+                if ((lyric[line.actualLine] == item) && (item != "%")) {
+                    HTMLLyrics += '<div class="row"><div class="col-sm-6"><input type="text" class="form-control" placeholder="' + lyricTimes[x * 2] + '"></input></div><div class="col-sm-6"><input type="text" class="form-control" placeholder="' + lyricTimes[x * 2 + 1] + '"></input></div></div>';
+                    HTMLLyrics += '<p class="active">' + item + '</p>';
+                } else if (item != "%") {
+                    HTMLLyrics += '<div class="row"><div class="col-sm-6"><input type="text" class="form-control" placeholder="' + lyricTimes[x * 2] + '"></input></div><div class="col-sm-6"><input type="text" class="form-control" placeholder="' + lyricTimes[x * 2 + 1] + '"></input></div></div>';
+                    HTMLLyrics += '<p>' + item + '</p>';
                 }
-
-                x++;
             });
 
             document.getElementById("lyrics").innerHTML = HTMLLyrics;
@@ -327,28 +321,17 @@
         function pause(value) {
             paused = value;
         }
-        
+
         function calculateInterval(start, end) {
             var interval = (end - start) * 10;
             return interval;
         }
 
         line.registerListener(function(value) {
-            goToLine(line.actualLine);            
+            goToLine(line.actualLine);
         });
     </script>
 
-    <script>
-        function search() {
-            // before: offset-md-3 offset-lg-3 col-sm-12 col-md-7 col-lg-6
-            // after: col-sm-12 col-md-7 col-lg-6
-            document.getElementById("lyric-column").classList.remove('offset-md-3');
-            document.getElementById("lyric-column").classList.remove('offset-lg-3');
-            
-            document.getElementById("video-column").style.display = "block";
-            document.getElementById("lyric-column").style.display = "block";
-        }
-    </script>
-
 </body>
+
 </html>
